b571514b5c9b2c45f00c26cec282128469e99eb4
Uday Prasad <Uday1260-lab>
2025-03-13 23:27:19 +0530

    Created Home page

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import cloudinary from "../lib/cloudinary.js";
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -14,26 +15,29 @@ export const getUsersForSidebar = async (req, res) => {
 
 
 export const getMessages = async (req, res) => {
-    try {
-        const {id:userToChatId} = req.params
-        const myId = req.user._id
-        const messages = await MessageChannel.find({
-            $or:[
-                {senderId: myId, receiverId: userToChatId},
-                {senderId: userToChatId, receiverId: myId}
-            ]
-        })
-        res.status(200).json(messages)
-    } catch (error) {
-        console.log("Error in getMessages: ", error.message)
-        res.status(500).json({error: "Internal Server Error"})
-    }
-}
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+    console.log(messages)
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 export const sendMessage = async (req, res) => {
     try {
         const {text, image} = req.body
-        const {id: receiverId} = req.params
+        const { id: receiverId } = req.params;
         const senderId = req.user._id
 
         let imageUrl;
@@ -42,12 +46,12 @@ export const sendMessage = async (req, res) => {
             imageUrl = uploadResponse.secure_url
         }
 
-        const newMessage = new MessageChannel({
-            senderId,
-            receiverId,
-            text,
-            image: imageUrl
-        })
+        const newMessage = new Message({
+          senderId,
+          receiverId,
+          text,
+          image: imageUrl,
+        });
         await newMessage.save()
         res.status(201).json(newMessage)
     } catch (error) {
