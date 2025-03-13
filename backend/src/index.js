31b0bfc7237ac9d0d65ad9dffd939a38a39e99a8
Uday Prasad <Uday1260-lab>
2025-03-13 20:15:33 +0530

    Created HomePage LoginPage SettingPage and UpdatePage

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 import { connectDB } from './lib/db.js'
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 dotenv.config()
 const PORT = process.env.PORT
@@ -11,11 +12,15 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/messages", messageRoutes)
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
     console.log("Server running at port: " + PORT)
     connectDB()
 })
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,47 @@
+import daisyui from "daisyui"
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",  // Adjust based on your project structure
-    "./public/index.html",  // Ensure Tailwind scans the HTML file (if applicable)
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust based on your project structure
+    "./public/index.html", // Ensure Tailwind scans the HTML file (if applicable)
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
 };
