63d1132e88801c652651ed54ec3797ae791b1d3b
Uday Prasad <Uday1260-lab>
2025-03-13 10:56:34 +0530

    Partially created backend operation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,22 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
+import { connectDB } from './lib/db.js'
+import cookieParser from "cookie-parser"
 
+dotenv.config()
+const PORT = process.env.PORT
 const app = express();
 
+app.use(express.json())
+app.use(cookieParser())
+
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(5001, () => {
-    console.log("Server running at port 5000")
+    console.log("Server running at port: " + PORT)
+    connectDB()
 })
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Adjust based on your project structure
+    "./public/index.html",  // Ensure Tailwind scans the HTML file (if applicable)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
